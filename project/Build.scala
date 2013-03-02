ba8c7af3da9f32de6f07fd380049eba9f83044de
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-03-02 13:30:09 +0900

    Added settings for publishing

--- project/Build.scala
+++ project/Build.scala
@@ -19,6 +19,41 @@ object Build extends Build {
         "play" %% "play" % "2.1.0" % "provided",
         "play" %% "play-test" % "2.1.0" % "test"
       )
-    )
+    ) ++ publishingSettings
   )
+
+  val publishingSettings = Seq(
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) => _publishTo(v) },
+    publishArtifact in Test := false,
+    pomExtra := _pomExtra
+  )
+
+  def _publishTo(v: String) = {
+    val nexus = "https://oss.sonatype.org/"
+    if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+    else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+  }
+
+  val _pomExtra =
+    <url>http://github.com/tototoshi/play-ascii-art-plugin</url>
+    <licenses>
+      <license>
+        <name>Apache License, Version 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:tototoshi/play-ascii-art-plugin.git</url>
+      <connection>scm:git:git@github.com:tototoshi/play-ascii-art-plugin.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>tototoshi</id>
+        <name>Toshiyuki Takahashi</name>
+        <url>http://tototoshi.github.com</url>
+      </developer>
+    </developers>
+
 }
